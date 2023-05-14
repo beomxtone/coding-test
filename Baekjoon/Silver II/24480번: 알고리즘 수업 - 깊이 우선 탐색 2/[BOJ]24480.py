701c91db4014726e01327938d058a3b3b72ef251
Seo BeomSeok <soulstair@gmail.com>
2023-05-14 14:34:43 +0900

    [BOJ]24480: 알고리즘 수업 - 깊이 우선 탐색 2 풀이

---
+++ Baekjoon/Silver II/24480번: 알고리즘 수업 - 깊이 우선 탐색 2/[BOJ]24480.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
+
+#
+# 24480번: 알고리즘 수업 - 깊이 우선 탐색 2
+# https://www.acmicpc.net/problem/24480
+#
+# @author  Asher Seo
+#
+
+def dfs(value):
+  global cnt
+  visited[value] = cnt
+  graph[value].sort(reverse=True)
+
+  for i in graph[value]:
+    if visited[i] == 0:
+      cnt += 1
+      dfs(i)
+
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
+cnt = 1
+
+for _ in range(m):
+  u, v = map(int, input().split())
+  graph[u].append(v)
+  graph[v].append(u)
+
+dfs(r)
+
+for i in range(1, n+1):
+  print(visited[i])
