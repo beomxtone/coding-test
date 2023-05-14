b11008b1399f571025fc628dae4bd1612ae6643d
Seo BeomSeok <soulstair@gmail.com>
2023-05-14 14:50:32 +0900

    [BOJ]24444: 알고리즘 수업 - 너비 우선 탐색 1 풀이

---
+++ Baekjoon/Silver II/24444번: 알고리즘 수업 - 너비 우선 탐색 1/[BOJ]24444.py
@@ -0,0 +1,40 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+#
+# 24444번: 알고리즘 수업 - 너비 우선 탐색 1
+# https://www.acmicpc.net/problem/24444
+#
+# @author  Asher Seo
+#
+
+def bfs(start):
+  global cnt
+  queue = deque([start])
+  visited[start] = cnt
+  cnt += 1
+
+  while queue:
+    v = queue.popleft()
+    graph[v].sort()
+    for i in graph[v]:
+      if visited[i] == 0:
+        queue.append(i)
+        visited[i] = cnt
+        cnt += 1
+
+n, m, r = map(int, input().split())
+graph = [[] * _ for _ in range(n+1)]
+visited = [0] * (n+1)
+cnt = 1
+
+for _ in range(m):
+  u, v = map(int, input().split())
+  graph[u].append(v)
+  graph[v].append(u)
+
+bfs(r)
+
+for i in range(1, n+1):
+  print(visited[i])
