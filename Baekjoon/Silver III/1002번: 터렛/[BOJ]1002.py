17d294163f2f62fa46c05363f0add043f484ce04
Asher Seo <beomxtone@gmail.com>
2023-06-22 10:21:04 +0900

    [BOJ]1002: 터렛 반례 추가

--- Baekjoon/Silver III/1002번: 터렛/[BOJ]1002.py
+++ Baekjoon/Silver III/1002번: 터렛/[BOJ]1002.py
@@ -6,6 +6,8 @@ import math
 # 1002번: 터렛
 # https://www.acmicpc.net/problem/1002
 #
+# 1. 두 사람의 위치가 같으면서, r1=r2=0인 경우 추가
+#
 # @author  Asher Seo
 #
 
@@ -15,7 +17,10 @@ for _ in range(TC):
   
   dis = math.sqrt((x1-x2)**2 + (y1-y2)**2)
   if dis == 0 and r1 == r2:
-    print(-1)
+    if r1 == 0:
+      print(1)
+    else:
+      print(-1)
   elif abs(r1-r2) < dis < (r1+r2):
     print(2)
   elif dis == abs(r1-r2) or dis == r1+r2:
