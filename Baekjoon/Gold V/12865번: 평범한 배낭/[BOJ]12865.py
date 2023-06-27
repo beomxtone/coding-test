92248974e9c66c2f46614323f833ed2e35c86fda
Asher Seo <beomxtone@gmail.com>
2023-06-27 18:05:44 +0900

    [BOJ]12865: 평범한 배낭 재채점 풀이

--- Baekjoon/Gold V/12865번: 평범한 배낭/[BOJ]12865.py
+++ Baekjoon/Gold V/12865번: 평범한 배낭/[BOJ]12865.py
@@ -13,18 +13,19 @@ input = sys.stdin.readline
 #
 
 n, k = map(int, input().split())
-dp = [[0]*(k+1) for _ in range(n)]
-items = []
+items = [(0, 0)]
 for _ in range(n):
   w, v = map(int, input().split())
   items.append((w, v))
 
-for i in range(n):
+dp = [[0]*(k+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+  w, v = items[i]
   for j in range(1, k+1):
-    weight, value = items[i]
-    if j < weight:
+    if j < w:
       dp[i][j] = dp[i-1][j]
     else:
-      dp[i][j] = max(value + dp[i-1][j-weight], dp[i-1][j])
+      dp[i][j] = max(dp[i-1][j], dp[i-1][j-w] + v)
 
 print(dp[-1][-1])
